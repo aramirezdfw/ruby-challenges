9b525fe5783a9989434759c77283915d4a64d6ac
Albert Ramirez <ownerbrown@Albert-MBP.attlocal.net>
2015-08-01 00:25:20 -0500

    My first commit

---
+++ weather.rb
@@ -0,0 +1,15 @@
+fav_day = 'sunny'
+case fav_day
+when 'rainy'
+puts "Don't forget your unbrella!"
+when 'cloudy'
+puts "You can still get a sunburn even if the sun is not out today."
+when 'foggy'
+puts "Use low bean headlights in the car."
+when 'windy'
+puts "Hang on to your hat!"
+when 'stormy'
+puts "Watch out for lightning!"
+else
+puts "Might as well stay home today!"
+end
